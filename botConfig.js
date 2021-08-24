66da525f982bf682cea6837b9b8b1c7887dd2d53
Razqa Dev <89305796+Iloilagi@users.noreply.github.com>
2021-08-24 10:09:14 +0700

    Delete botConfig.js

--- botConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    token: "ODc4MTAyNDQ2Njc1NzM4NzE1.YR8SoQ.O_G_m7nu4OZYNaF8Ofvg2ZaU6c0",
-    prefix: ".",
-    admins: [
-        "821627699999145995"
-],
-    debug: true,
-    countChannel: "countChannelID"
-};
